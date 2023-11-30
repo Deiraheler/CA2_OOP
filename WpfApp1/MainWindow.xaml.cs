d50cd03779a98d1a05a263bdff4efa0eb74482a3
Deiraheler <53556704+Deiraheler@users.noreply.github.com>
2023-11-30 12:50:19 +0000

    XML ready. ListBoxes selection is working now

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -16,10 +16,76 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        //List with all Teams
+        List<Team> Teams = new List<Team>();
+
         public MainWindow()
         {
             InitializeComponent();
 
+            InitTeams();
+
+            teamsList.ItemsSource = Teams;
+            teamsList.DisplayMemberPath = "Name";
+        }
+
+        private void InitTeams()
+        {
+            Teams.AddRange(new List<Team>{
+                new Team() { Name = "France" },
+                new Team() { Name = "Italy" },
+                new Team() { Name = "Spain" },
+            });
+
+            //Adding players for France
+            Teams[0].Players.AddRange(new List<Player>
+            {
+                new Player { Name = "Marie", ResultRecord = "WWDDL" },
+                new Player { Name = "Claude", ResultRecord = "DDDLW" },
+                new Player { Name = "Antoine", ResultRecord = "LWDLW" }
+            });
+
+            //Adding players for Italy
+            Teams[1].Players.AddRange(new List<Player>
+            {
+                new Player() { Name = "Marco", ResultRecord = "WWDLL" },
+                new Player() { Name = "Giovanni", ResultRecord = "LLLLD" },
+                new Player() { Name = "Valentina", ResultRecord = "DLWWW" }
+            });
+
+            //Adding players for Spain
+            Teams[2].Players.AddRange(new List<Player>
+            {
+                new Player() { Name = "Maria", ResultRecord = "WWWWW" },
+                new Player() { Name = "Jose", ResultRecord = "LLLLL" },
+                new Player() { Name = "Pablo", ResultRecord = "DDDDD" }
+            });
+        }
+
+        private void teamsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (teamsList.SelectedIndex != -1)
+            {
+                Team selectedTeam = teamsList.SelectedItem as Team;
+
+                if (selectedTeam != null)
+                {
+                    playersList.ItemsSource = selectedTeam.Players;
+                }
+            }
+        }
+
+        private void playersList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (teamsList.SelectedIndex != -1)
+            {
+                Team selectedTeam = teamsList.SelectedItem as Team;
+
+                if (selectedTeam != null)
+                {
+                    playersList.ItemsSource = selectedTeam.Players;
+                }
+            }
         }
     }
 }
--- WpfApp1/class/Player.cs
+++ WpfApp1/class/Player.cs
@@ -7,6 +7,17 @@ using System.Threading.Tasks;
 namespace WpfApp1;
     internal class Player
     {
-        
+    public string Name { get; set; }
+    public string ResultRecord { get; set; }
+
+    public int GetPoints()
+    {
+        return ResultRecord;
     }
+
+    public override string ToString()
+    {
+        return ResultRecord;
+    }
+}
 
